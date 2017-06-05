c5de156f2af67e2aee24fdf955e17bd16ab98f34
drw <drw@users.noreply.github.com>
2017-06-05 15:53:59 -0400

    Reflow comments

--- pull_last_hour.py
+++ pull_last_hour.py
@@ -1,16 +1,18 @@
-# This script can be run to pull the last hour of parking data through the process_data 
-# script and (with the argument push_to_CKAN = True in that function call), cause the
-# resulting data to be pushed to the CKAN instance specified by modules and settings
-# called by process_data.
+# This script can be run to pull the last hour of parking data through 
+# the process_data script and (with the argument push_to_CKAN = True in 
+# that function call), cause the resulting data to be pushed to the 
+# CKAN instance specified by modules and settings called by process_data.
 
-# The main() function returns a Boolean indicating whether this operation succeeded or
-# failed. In this way, this function can still be called by some kind of pipeline/job
-# manager that can send out notifications if a particular ETL job fails.
+# The main() function returns a Boolean indicating whether this operation 
+# succeeded or failed. In this way, this function can still be called by 
+# some kind of pipeline/job manager that can send out notifications if a 
+# particular ETL job fails.
 
 
-# An intermediate step between making pipe_data_to_CKAN work and this function
-# would be running this script with output_to_csv = True and push_to_CKAN = False
-# and then calling a pipeline function to pipe the CSV to the CKAN resource.
+# An intermediate step between making pipe_data_to_CKAN work and this 
+# function would be running this script with output_to_csv = True and 
+# push_to_CKAN = False and then calling a pipeline function to pipe the 
+# CSV to the CKAN resource.
 import pytz
 from datetime import datetime, timedelta
 import process_data
