6ea331180f9f2436ba72426c90ce63aa1eea7fc7
drw <drw@users.noreply.github.com>
2017-06-06 13:07:19 -0400

    Make initialize_datastores more verbose, explicit, and user-friendly

--- prime_ckan/initialize_datastores.py
+++ prime_ckan/initialize_datastores.py
@@ -1,46 +1,54 @@
 import json
 from datapusher import Datapusher
 from remote_parameters import resource_id, ad_hoc_resource_id
-
-print("Insert a 'Do you really want to do this?' prompt here.")
-
-with open('ckan_settings.json') as f:
-    settings = json.load(f)
+from gadgets import query_yes_no, fire_grappling_hook, get_resource_name, get_package_name_from_resource_id
 
 server="Live"
-dp = Datapusher(settings, server=server)
-dp.delete_datastore(resource_id)
-ordered_fields = [{"id": "Zone", "type": "text"}]
-ordered_fields.append({"id": "Start", "type": "timestamp"})
-ordered_fields.append({"id": "End", "type": "timestamp"})
-ordered_fields.append({"id": "UTC Start", "type": "timestamp"})
-ordered_fields.append({"id": "Transactions", "type": "int"})
-ordered_fields.append({"id": "Car-minutes", "type": "int"})
-ordered_fields.append({"id": "Payments", "type": "float"})
-ordered_fields.append({"id": "Durations", "type": "json"})
+site, API_key, settings = fire_grappling_hook('ckan_settings.json',server)
 
-keys = ["Zone", "UTC Start"]
-call_result = dp.create_datastore(resource_id, ordered_fields, keys=keys)
-print("Datastore creation result: {}".format(call_result))
-###############################################################
+resource_name = get_resource_name(site,resource_id,API_key)
+ad_hoc_resource_name = get_resource_name(site,ad_hoc_resource_id,API_key)
+package_name = get_package_name_from_resource_id(site,resource_id,API_key)
 
-with open('ckan_settings.json') as f:
-    settings = json.load(f)
+question = "Do you really want to initalize (and possibly delete) these datastores? "
+question += "({} ({}) and {} ({}), both from {})"
+question = question.format(resource_name,resource_id,ad_hoc_resource_name,ad_hoc_resource_id, package_name)
 
-server="Live"
-dp = Datapusher(settings, server=server)
-dp.delete_datastore(ad_hoc_resource_id)
-ordered_fields = [{"id": "Zone", "type": "text"}]
-ordered_fields.append({"id": "Parent Zone", "type": "text"})
-ordered_fields.append({"id": "Start", "type": "timestamp"})
-ordered_fields.append({"id": "End", "type": "timestamp"})
-ordered_fields.append({"id": "UTC Start", "type": "timestamp"})
-ordered_fields.append({"id": "Transactions", "type": "int"})
-ordered_fields.append({"id": "Car-minutes", "type": "int"})
-ordered_fields.append({"id": "Payments", "type": "float"})
-ordered_fields.append({"id": "Durations", "type": "json"})
+response = query_yes_no(question, "no")
 
 
-keys = ["Zone", "UTC Start"]
-call_result = dp.create_datastore(ad_hoc_resource_id, ordered_fields, keys=keys)
-print("Datastore creation result: {}".format(call_result))
+if response:
+    dp = Datapusher(settings, server=server)
+    dp.delete_datastore(resource_id)
+    ordered_fields = [{"id": "Zone", "type": "text"}]
+    ordered_fields.append({"id": "Start", "type": "timestamp"})
+    ordered_fields.append({"id": "End", "type": "timestamp"})
+    ordered_fields.append({"id": "UTC Start", "type": "timestamp"})
+    ordered_fields.append({"id": "Transactions", "type": "int"})
+    ordered_fields.append({"id": "Car-minutes", "type": "int"})
+    ordered_fields.append({"id": "Payments", "type": "float"})
+    ordered_fields.append({"id": "Durations", "type": "json"})
+
+    keys = ["Zone", "UTC Start"]
+    call_result = dp.create_datastore(resource_id, ordered_fields, keys=keys)
+    print("Datastore creation result: {}".format(call_result))
+    ###############################################################
+
+    dp = Datapusher(settings, server=server)
+    dp.delete_datastore(ad_hoc_resource_id)
+    ordered_fields = [{"id": "Zone", "type": "text"}]
+    ordered_fields.append({"id": "Parent Zone", "type": "text"})
+    ordered_fields.append({"id": "Start", "type": "timestamp"})
+    ordered_fields.append({"id": "End", "type": "timestamp"})
+    ordered_fields.append({"id": "UTC Start", "type": "timestamp"})
+    ordered_fields.append({"id": "Transactions", "type": "int"})
+    ordered_fields.append({"id": "Car-minutes", "type": "int"})
+    ordered_fields.append({"id": "Payments", "type": "float"})
+    ordered_fields.append({"id": "Durations", "type": "json"})
+
+
+    keys = ["Zone", "UTC Start"]
+    call_result = dp.create_datastore(ad_hoc_resource_id, ordered_fields, keys=keys)
+    print("Datastore creation result: {}".format(call_result))
+else:
+    print("OK, no changes were made.")
