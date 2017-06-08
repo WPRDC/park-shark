2962dc0a4428d7b465131f708cebcfde74ce7d52
drw <drw@users.noreply.github.com>
2017-06-08 16:37:00 -0400

    Add untested functions to cache purchases in memory

--- process_data.py
+++ process_data.py
@@ -64,6 +64,10 @@ last_date_cache = None
 all_day_ps_cache = []
 dts = []
 
+last_utc_date_cache = None
+utc_ps_cache = []
+utc_dts = []
+
 temp_zone_info = {'344 - 18th & Carson Lot': {'Latitude': 40.428484093957401,
                  'Longitude': -79.98027965426445,
                  'MeterCount': 2,
@@ -298,7 +302,7 @@ def roundTime(dt=None, roundTo=60):
 
 def beginning_of_day(dt=None):
     # Takes a datetime and returns the first datetime before
-    # that that corresponds to local midnight (00:00).
+    # that that corresponds to LOCAL midnight (00:00).
     if dt == None : dt = datetime.now()
     return dt.replace(hour=0, minute=0, second=0, microsecond=0)
 
@@ -850,6 +854,284 @@ def in_db(cached_dates,date_i):
 # end database functions #
 
 
+def placeholder():
+        global last_utc_date_cache, utc_ps_cache, utc_ts
+        if last_utc_date_cache != slot_start_date_string:
+            if not mute:
+                print("last_utc_date_cache ({}) doesn't match slot_start_date_string ({})".format(last_utc_date_cache, slot_start_date_string))
+
+            utc_ps_all = []
+            dt_start_i = slot_start
+            while dt_start_i < slot_end:
+                ps_for_whole_utc_day = get_batch_parking_for_day(dt_start_i,cache,mute)
+                ps_all += ps_for_whole_utc_day
+                dt_start_i += timedelta(days = 1)
+                if not mute:
+                    print("Now there are {} transactions in ps_all".format(len(ps_all)))
+
+            all_day_ps_cache = ps_all # Note that if slot_start and slot_end are not on the same day,
+            # all_day_ps_cache will hold transactions for more than just the date of slot_start, but
+            # since filtering is done further down in this function, this should not represent a
+            # problem. There should be no situations where more than two days of transactions will
+            # wind up in this cache at any one time.
+            dts = [tz.localize(datetime.strptime(p[time_field],dt_format)) for p in ps_all]
+            time.sleep(3)
+        else:
+            utc_ps_all = utc_ps_cache
+        #ps = [p for p in ps_all if slot_start <= tz.localize(datetime.strptime(p[time_field],'%Y-%m-%dT%H:%M:%S')) < slot_end] # This takes like 3 seconds to
+        # execute each time for busy days since the time calculations
+        # are on the scale of tens of microseconds.
+        # So let's generate the datetimes once (above), and do
+        # it this way:
+        ps = [p for p,utc_dt in zip(utc_ps_all,utc_dts) if slot_start <= utc_dt < slot_end]
+        # Now instead of 3 seconds it takes like 0.03 seconds.
+        last_utc_date_cache = slot_start_date_string
+
+# Attempting to split get_ps_from_somewhere into two functions to enable day-by-day
+# caching, to reduce database hits, like in the previous approach.
+
+def get_ps_for_day(db,slot_start,cache=True,mute=False):
+    # Caches parking once it's been downloaded (in a database) and checks
+    # cache before redownloading.
+
+    # Note that no matter what time of day is associated with slot_start,
+    # this function will get all of the transactions for that entire day.
+    # Filtering the results down to the desired time range is handled 
+    # elsewhere (in the calling function (get_batch_parking)).
+    ###############
+    # This function gets parking purchases, either from a 
+    # cache database (if the date appears to have been cached)
+    # or else from the API (and then caches the whole thing 
+    # if cache = True).
+
+    # Note that no matter what time of day is associated with slot_start,
+    # this function will get all of the transactions for that entire day.
+
+    # Filtering the results down to the desired time range is now handled 
+    # in this functions (though the function only marks a date as cached
+    # when it has added all events from the day of slot_start (based on
+    # StartDateUtc)).
+    
+    # This approach tries to address the problem of the often 
+    # gigantic discrepancy between the DateCreatedUtc timestamp
+    # and the StartDateUtc timestamp.
+
+    # This new database-centric, StartDateUtc-based approach has
+    # been verified to give the same results as the old approach
+    # for 2012-07-23.
+
+    cached_dates,cached_ps = get_tables_from_db(db)
+
+    ref_field = '@StartDateUtc' # This should not be changed.
+    date_format = '%Y-%m-%d'
+
+    tz = pytz.utc
+    ps_all = []
+    dts_all = []
+    #for each date in day before, day of, and day after (to get all transactions)
+        # [Three full days is probably slightly overkill since the most extreme 
+        # negative case of DateCreatedUtc - StartDateUtc observed so far has 
+        # been -12 hours.]
+
+    # To avoid issues with missing or extra hours during Daylight Savings Time,
+    # all slot times should be in UTC.
+
+    # The cached_dates format is also UTC dates. (This change was made to 
+    # route around issues encountered when using localized versions of 
+    # StartDateUtc and converting to dates.)
+
+    slot_start_date_string = slot_start.astimezone(tz).strftime(date_format) # This is the date string 
+    # for the start of the overall desired time range.
+
+    if cached_dates.find_one(date=slot_start_date_string) is None:
+        # A date is added to cached_dates when the date and the two surrounding it have
+        # been queried and all events with a StartDateUTC value in the day corresponding
+        # to slot_start (as well as slot_start_date_string) have been added to the database.
+
+        # That way, if the date is in cached_dates, the query can be done on the 
+        # database without hitting the API.
+
+        if not mute:
+            print("Sigh! {} not found in cached_dates, so I'm pulling the data from the API...".format(slot_start_date_string))
+       # Pull the data from the API.
+        for offset in range(-1,2):
+            query_start = beginning_of_day(slot_start) + (offset)*timedelta(days = 1)
+            query_end = query_start + timedelta(days = 1)
+
+            #query_date_string = query_start.astimezone(tz).strftime(date_format) # This is the date
+            # in the local time zone. # This seems to not be used at all.
+
+            base_url = 'http://webservice.mdc.dmz.caleaccess.com/cwo2exportservice/BatchDataExport/4/BatchDataExport.svc/purchase/ticket/'
+            url = build_url(base_url,query_start,query_end)
+
+            if not mute:
+                print("Here's the URL for offset = {}: {}".format(offset,url))
+            r = requests.get(url, auth=(CALE_API_user, CALE_API_password))
+
+            # Convert Cale's XML into a Python dictionary
+            doc = xmltodict.parse(r.text,encoding = r.encoding)
+
+            url2 = doc['BatchDataExportResponse']['Url']
+            r2 = requests.get(url2, auth=(CALE_API_user, CALE_API_password))
+            doc = xmltodict.parse(r2.text,encoding = r2.encoding)
+
+            while not r2.ok or doc['BatchDataExportFileResponse']['ExportStatus'] != 'Processed':
+                time.sleep(10)
+                r2 = requests.get(url2, auth=(CALE_API_user, CALE_API_password))
+                doc = xmltodict.parse(r2.text,encoding = r2.encoding)
+
+            url3 = doc['BatchDataExportFileResponse']['Url']
+
+            # When the ZIP file is ready:
+            r3 = requests.get(url3, stream=True, auth=(CALE_API_user, CALE_API_password))
+            while not r3.ok:
+                time.sleep(5)
+                r3 = requests.get(url3, stream=True, auth=(CALE_API_user, CALE_API_password))
+
+            z = zipfile.ZipFile(BytesIO(r3.content))
+
+            # Extract contents of a one-file zip file to memory:
+            xml = z.read(z.namelist()[0])
+            doc = xmltodict.parse(xml,encoding = 'utf-8')
+
+            ps = convert_doc_to_purchases(doc['BatchExportRoot'],query_start,date_format)
+
+            # Remove fields #
+            purchases = cull_fields(ps)
+
+            # Filter down to the events in the slot, adding on two date/time fields #
+            datetimes = [(pytz.utc).localize(datetime.strptime(p[ref_field],'%Y-%m-%dT%H:%M:%S')) for p in purchases]
+            #ps = [p for p,dt in zip(purchases,dts) if beginning_of_day(slot_start) <= dt < beginning_of_day(slot_start) + timedelta(days=1)]
+            ps = []
+            dts = []
+            
+            start_of_day = beginning_of_day(slot_start)
+            start_of_next_day = beginning_of_day(slot_start) + timedelta(days=1)
+            for purchase_i,datetime_i in zip(purchases,datetimes):
+                if start_of_day <= datetime_i < start_of_next_day:
+                    purchase_i['StartDateUTC_date'] = (datetime_i).astimezone(tz).strftime(date_format) # This SHOULD be equal to slot_start.date().........
+                    # but verify this.
+                    # [ ] Assuming that these two things are equal would speed up processing a little.
+                    if purchase_i['StartDateUTC_date'] != slot_start_date_string:
+                        print("slot_start = {} = {}".format(slot_start, slot_start.astimezone(pytz.utc)))
+                        print("slot_start_date_string (This is the local date) = {}".format(slot_start_date_string))
+                        print("beginning_of_day(slot_start) = {}".format(start_of_day))
+                        print("datetime_i = {}".format(datetime_i))
+                        print("beginning_of_day(slot_start) + timedelta(days=1) = {}".format(start_of_next_day))
+                        pprint.pprint(purchase_i)
+                        raise ValueError("purchase_i['StartDateUTC_date'] != slot_start_date_string, {} != {}".format(purchase_i['StartDateUTC_date'], slot_start_date_string))
+                    ps.append(purchase_i)
+                    dts.append(datetime_i)
+
+            for p,dt in zip(ps,dts):
+                p['unix_time'] = epoch_time(dt)
+                # This is a hack to provide a float that can be stored in SQLite (which has serious problems with datetime 
+                # comparisons) until I can get a Postgres database set up.
+            
+            # Add these purchases to the overall accumulating set that fit in the slot #
+            ps_all += ps #ps_all is all purchases that have StartDateUtc values between the beginning of the day corresponding to slot_start
+            # and the beginning of the next day (24 hours later, in UTC).
+            dts_all += dts         
+        
+        if cache:
+            # Store in db and update cached_dates.
+
+            ps_all_fixed = remove_field(ps_all,'PurchasePayUnit') # PurchasePayUnit itself contains a data structure
+            # so dataset can't handle sticking it into a databse.
+            # It might be better to take the payment information fields and add them as scalar fields.
+
+            # Verify that there are currently no transactions in the database with the target StartDateUTC date string:
+            should_be_none = cached_ps.find_one(StartDateUTC__localized_date = slot_start_date_string)
+
+            if should_be_none is not None:
+                print("should_be_none = ")
+                pprint.pprint(should_be_none)
+                raise ValueError("A transaction was found in the database even though it shouldn't have been there according to cached_dates.")
+
+            # ps is a list of dicts.
+            t_x = time.time()
+
+            db.begin()
+            try:
+                for p in ps_all_fixed:
+                    db['cached_purchases'].upsert(p, ['@PurchaseGuid'])
+                db.commit()
+            except:
+                db.rollback()
+            # The upserting code was rewritten from the version below to the version above 
+            # to try to speed up the process, but it's not clear how much it really helped.
+            # Both would need to be tested on a list of ~10,000 transactions to see the 
+            # true improvement.
+
+            #for p in ps_all_fixed:
+            #    cached_ps.upsert(p, ['@PurchaseGuid'])
+            t_y = time.time()
+            print("     Time required to upsert {} transactions to the database: {} s".format(len(ps_all_fixed),t_y-t_x))
+
+            cached_dates.upsert(dict(date = slot_start_date_string), ['date'])
+    else: # Load locally cached data
+        start_of_day = beginning_of_day(slot_start)
+        start_of_next_day = beginning_of_day(slot_start) + timedelta(days=1)
+        start_epoch = epoch_time(start_of_day)
+        next_epoch = epoch_time(start_of_next_day)
+        ps = list(db.query("SELECT * FROM cached_purchases WHERE unix_time >= {} and unix_time < {}".format(start_epoch,next_epoch)))
+        # Manual tests suggest that the unix_time query is at least not returning any results outside the intended date range
+
+        ps2 = list(db.query("SELECT * FROM cached_purchases WHERE StartDateUTC_date = {}".format(slot_start_date_string)))
+        print("The unix_time query returned {} transactions, while the StartDateUtc_date query (seeking {}) returned {} transactions.".format(len(ps), slot_start_date_string, len(ps2)))
+
+    return ps
+
+def get_batch_parking(slot_start,slot_end,cache,mute=False,tz=pytz.utc,time_field = '@StartDateUtc',dt_format='%Y-%m-%dT%H:%M:%S.%f'):
+    # This function handles situation where slot_start and slot_end are on different days
+    # by calling get_ps_for_day in a loop.
+
+    # The parameter "time_field" determines which of the timestamps is used for calculating
+    # the datetime values used to filter purchases down to those between slot_start
+    # and start_end.
+
+
+    # Note that the time zone tz and the time_field must be consistent for this to work properly.
+    # Here is a little sanity check:
+    
+    if (re.search('Utc',time_field) is not None) != (tz == pytz.utc): 
+        # This does an XOR between these values.
+        raise RuntimeError("It looks like the time_field may not be consistent with the provided time zone")
+
+    global last_utc_date_cache, utc_ps_cache, utc_dts_cache
+    if last_utc_date_cache != slot_start.date():
+        if not mute:
+            print("last_utc_date_cache ({}) doesn't match slot_start.date() ({})".format(last_utc_date_cache, slot_start.date()))
+
+        ps_all = []
+        dt_start_i = slot_start
+        while dt_start_i < slot_end:
+            ps_for_whole_day = get_ps_for_day(db,dt_start_i,cache,mute)
+            ps_all += ps_for_whole_day
+            dt_start_i += timedelta(days = 1)
+            if not mute:
+                print("Now there are {} transactions in ps_all".format(len(ps_all)))
+
+        utc_ps_cache = ps_all # Note that if slot_start and slot_end are not on the same day,
+        # utc_ps_cache will hold transactions for more than just the date of slot_start, but 
+        # since filtering is done further down in this function, this should not represent a 
+        # problem. There should be no situations where more than two days of transactions will
+        # wind up in this cache at any one time.
+        utc_dts_cache = [tz.localize(datetime.strptime(p[time_field],dt_format)) for p in ps_all] # This may break for StartDateUtc!!!!!
+        time.sleep(2)
+    else:
+        ps_all = utc_ps_cache
+    #ps = [p for p in ps_all if slot_start <= tz.localize(datetime.strptime(p[time_field],'%Y-%m-%dT%H:%M:%S')) < slot_end] # This takes like 3 seconds to
+    # execute each time for busy days since the time calculations
+    # are on the scale of tens of microseconds.
+    # So let's generate the datetimes once (above), and do
+    # it this way:
+    ps = [p for p,dt in zip(ps_all,utc_dts_cache) if slot_start <= dt < slot_end]
+    # Now instead of 3 seconds it takes like 0.03 seconds.
+    last_utc_date_cache = slot_start.date()
+    return ps
+
+
 def get_ps_from_somewhere(db,slot_start,slot_end,cache=True,mute=False):
     # This function gets parking purchases, either from a 
     # cache database (if the date appears to have been cached)
@@ -1047,8 +1329,8 @@ def get_ps_from_somewhere(db,slot_start,slot_end,cache=True,mute=False):
     else: # Load locally cached version
         t_b = time.time()
         requested_ps = list(db.query("SELECT * FROM cached_purchases WHERE unix_time >= {} and unix_time < {}".format(epoch_time(slot_start),epoch_time(slot_end))))
-        print("t_c-t_b = {}".format(time.time()-t_b))
-
+        t_c = time.time()
+        print("Got {} transactions from the database in {} s.".format(t_c-t_b))
     return requested_ps
 
 def get_events_from_db(db,slot_start,slot_end,cache,mute=False,tz=pytz.timezone('US/Eastern'),time_field = '@PurchaseDateLocal',dt_format='%Y-%m-%dT%H:%M:%S'):
