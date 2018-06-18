3b431b6098587071dfe928030635c7b686f8e4e9
drw <drw@users.noreply.github.com>
2018-06-17 22:01:43 -0400

    Delete pipe/local_config.py as part of consolidation

--- pipe/local_config.py
+++
@@ -1,2 +0,0 @@
-SETTINGS_FILE = '/Users/drw/WPRDC/datasets/Parking-Data__public/parameters/transmogrifier_settings.json'
-#SETTINGS_FILE = '/Users/drw/WPRDC/datasets/Parking-Data__public/prime_ckan/ckan_settings.json'
