8f3e4b3e333123c91200d49448024db91c62b9f9
drw <drw@users.noreply.github.com>
2018-11-13 14:20:26 -0500

    Add simple script to clear the SQLite cached_dates table and corresponding files for a given date

---
+++ clear_date.py
@@ -0,0 +1,18 @@
+"""A simple script to clear the SQLite cache (both cached_dates
+table and SQLite files) for a single given date."""
+import sys
+from util.sqlite_util import clear_cache_for_date
+from dateutil import parser
+
+from parameters.local_parameters import path
+
+reference_time = 'purchase_time'
+
+if len(sys.argv) == 2:
+    dt = parser.parse(sys.argv[1])
+    date_i = dt.date()
+    print("Attempting to clear the SQLite cache for date = {}".format(date_i))
+    clear_cache_for_date(path,reference_time,date_i)
+    print("SQLite cache cleared for {}".format(date_i))
+else:
+    raise ValueError("Incorrect number of command-line parameters found.")
