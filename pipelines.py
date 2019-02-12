65c4496db33a133005a3907f29d8f9b1e637fca5
drw <drw@users.noreply.github.com>
2019-02-12 17:40:22 -0500

    Pare down datetimes pulled from the API further to just the effective installation date

--- pipelines.py
+++ pipelines.py
@@ -46,12 +46,12 @@ class MetersSchema(pl.BaseSchema):
     #cost_per_hour = fields.Float(dump_to='cost_per_hour',allow_none=True)
     #rate_information = fields.String(dump_to='rate_information',allow_none=True)
     #restrictions = fields.String(dump_to='restrictions',allow_none=True)
-    created_utc = fields.DateTime(dump_to='created_utc',allow_none=True) # This works
-    # because Marshmallow can take an ISO-formatted datetime and turn it into a DateTime field.
-    #active_utc = fields.DateTime(dump_to='active_utc',allow_none=True)
-    in_service_utc = fields.DateTime(dump_to='in_service_utc',allow_none=True)
+    #created_utc = fields.DateTime(dump_to='created_utc',allow_none=True) # created_utc is not as good a match for the Install Date field from the mast list as in_service_utc.
+    #active_utc = fields.DateTime(dump_to='active_utc',allow_none=True) # This works because Marshmallow can take an ISO-formatted datetime and turn it into a DateTime field.
+    in_service_utc = fields.DateTime(dump_to='in_service_utc',allow_none=True) # Best matches the Install Date field of the master list.
     #inactive_utc = fields.DateTime(dump_to='inactive_utc',allow_none=True)
-    removed_utc = fields.DateTime(dump_to='removed_utc',allow_none=True)
+    #removed_utc = fields.DateTime(dump_to='removed_utc',allow_none=True) # removed_utc is not complete enough in the CALE API. 405024-BUTLER3602 was removed (with
+    # a removal date given by the master list, but no removal date is present in the API.
 
     class Meta:
         ordered = True
