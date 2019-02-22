2a1967b3b319a9f7ff6912cf27b8ca07dbfffd32
drw <drw@users.noreply.github.com>
2019-02-22 14:46:19 -0500

    Add push_day_old_data.py to keep pushing data from 24 to 48 hours ago to CKAN

---
+++ push_day_old_data.py
@@ -0,0 +1,54 @@
+# This script can be run to read 24-hours of day-old parking data 
+# through the process_data script and push the resulting data to the 
+# CKAN instance specified by modules and settings called by process_data.
+
+import pytz, requests
+from datetime import datetime, timedelta
+from dateutil import parser
+import process_data
+import sys
+from pprint import pprint
+
+from util.util import get_terminals
+from read_entire_history import main, list_of_servers
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        args = sys.argv[1:]
+        output_to_csv = False
+        push_to_CKAN = True
+
+        copy_of_args = list(args)
+
+        pgh = pytz.timezone('US/Eastern')
+        current_hour = process_data.round_time(datetime.now(),60*60,"down") # Round current time down to the beginning of the hour.
+
+        kwparams = {}
+        slot_start = pgh.localize(current_hour - timedelta(days=2))
+        kwparams['slot_start'] = slot_start
+        halting_time = pgh.localize(current_hour - timedelta(days=1))
+        kwparams['halting_time'] = halting_time
+        print("slot_start = {}, halting_time = {}".format(slot_start, halting_time))
+
+        # This is a new way of parsing command-line arguments that cares less about position
+        # and just does its best to identify the user's intent.
+        for k,arg in enumerate(copy_of_args):
+            if arg in ['scan', 'save', 'csv']:
+                output_to_csv = True
+                args.remove(arg)
+            elif arg in ['pull', 'push', 'ckan']:
+                push_to_CKAN = True
+                args.remove(arg)
+            elif arg in list_of_servers:
+                kwparams['server'] = arg
+                args.remove(arg)
+            else:
+                print("I have no idea what do with args[{}] = {}.".format(k,arg))
+
+        kwparams['output_to_csv'] = output_to_csv
+        kwparams['push_to_CKAN'] = push_to_CKAN
+        pprint(kwparams)
+        main(**kwparams)
+    else:
+        raise ValueError("Please specify some command-line parameters")
+
