975d5e8cda7a099f10c8190082307cf1fa86eff7
drw <drw@users.noreply.github.com>
2019-02-22 14:55:56 -0500

    Catch push_day_old_data.py exceptions and print out traceback

--- push_day_old_data.py
+++ push_day_old_data.py
@@ -2,53 +2,61 @@
 # through the process_data script and push the resulting data to the 
 # CKAN instance specified by modules and settings called by process_data.
 
-import pytz, requests
+import sys, pytz, requests, traceback
 from datetime import datetime, timedelta
 from dateutil import parser
-import process_data
-import sys
 from pprint import pprint
 
+import process_data
 from util.util import get_terminals
 from read_entire_history import main, list_of_servers
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        args = sys.argv[1:]
-        output_to_csv = False
-        push_to_CKAN = True
+        try:
+            args = sys.argv[1:]
+            output_to_csv = False
+            push_to_CKAN = True
 
-        copy_of_args = list(args)
+            copy_of_args = list(args)
 
-        pgh = pytz.timezone('US/Eastern')
-        current_hour = process_data.round_time(datetime.now(),60*60,"down") # Round current time down to the beginning of the hour.
+            pgh = pytz.timezone('US/Eastern')
+            current_hour = process_data.round_time(datetime.now(),60*60,"down") # Round current time down to the beginning of the hour.
 
-        kwparams = {}
-        slot_start = pgh.localize(current_hour - timedelta(days=2))
-        kwparams['slot_start'] = slot_start
-        halting_time = pgh.localize(current_hour - timedelta(days=1))
-        kwparams['halting_time'] = halting_time
-        print("slot_start = {}, halting_time = {}".format(slot_start, halting_time))
+            kwparams = {}
+            slot_start = pgh.localize(current_hour - timedelta(days=2))
+            kwparams['slot_start'] = slot_start
+            halting_time = pgh.localize(current_hour - timedelta(days=1))
+            kwparams['halting_time'] = halting_time
+            print("slot_start = {}, halting_time = {}".format(slot_start, halting_time))
 
-        # This is a new way of parsing command-line arguments that cares less about position
-        # and just does its best to identify the user's intent.
-        for k,arg in enumerate(copy_of_args):
-            if arg in ['scan', 'save', 'csv']:
-                output_to_csv = True
-                args.remove(arg)
-            elif arg in ['pull', 'push', 'ckan']:
-                push_to_CKAN = True
-                args.remove(arg)
-            elif arg in list_of_servers:
-                kwparams['server'] = arg
-                args.remove(arg)
-            else:
-                print("I have no idea what do with args[{}] = {}.".format(k,arg))
+            # This is a new way of parsing command-line arguments that cares less about position
+            # and just does its best to identify the user's intent.
+            for k,arg in enumerate(copy_of_args):
+                if arg in ['scan', 'save', 'csv']:
+                    output_to_csv = True
+                    args.remove(arg)
+                elif arg in ['pull', 'push', 'ckan']:
+                    push_to_CKAN = True
+                    args.remove(arg)
+                elif arg in list_of_servers:
+                    kwparams['server'] = arg
+                    args.remove(arg)
+                else:
+                    print("I have no idea what do with args[{}] = {}.".format(k,arg))
 
-        kwparams['output_to_csv'] = output_to_csv
-        kwparams['push_to_CKAN'] = push_to_CKAN
-        pprint(kwparams)
-        main(**kwparams)
+            kwparams['output_to_csv'] = output_to_csv
+            kwparams['push_to_CKAN'] = push_to_CKAN
+            pprint(kwparams)
+            main(**kwparams)
+        except:
+            e = sys.exc_info()[0]
+            msg = "Error: {} : \n".format(e)
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            msg = ''.join('!! ' + line for line in lines)
+            msg = 'push_day_old_data.py: ' + msg
+            print(msg) # Log it or whatever here
     else:
         raise ValueError("Please specify some command-line parameters")
 
