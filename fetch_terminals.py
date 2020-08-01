a53d876782f654a95fd2beddf64210787e9ce000
drw <drw@users.noreply.github.com>
2020-07-31 22:18:57 -0400

    Ignoring Flowbird App terminals for now

--- fetch_terminals.py
+++ fetch_terminals.py
@@ -189,7 +189,11 @@ def pull_terminals(*args, **kwargs):
     print("Figure out what to do with the GARAGE zone and associated terminals.")
 
     ids_to_ignore = ['Friendship Ave RPP', '209001-MONWHARF', '213001-2NDAVEPZA'] # These are terminal IDs which should not be saved to CSV files or pushed to CKAN repositories.
+    flowbird_ids_to_ignore_for_now = [t['@Id'] for t in terminals if len(t['@Id']) <= 5]
+    print("Currently ignoring these terminal IDs: {}".format(flowbird_ids_to_ignore_for_now))
+    ids_to_ignore += flowbird_ids_to_ignore_for_now
     for k,t in enumerate(terminals):
+
         new_entry = {}
         new_entry['GUID'] = t['@Guid']
         new_entry['ID'] = t['@Id']
